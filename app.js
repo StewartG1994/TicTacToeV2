ed1b542e21dae40498eec7ac79048abdc7a994fc
Stewart Gliniecki <stewgliniecki@gmail.com>
2022-08-28 20:44:12 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const playerFactory = (name, type) =>{
+    return {name ,type}
+}
+
+const gameboard = (() =>{
+
+    const winningSequences = 
+    [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+
+
+    return {
+        winningSequences
+    }
+
+
+})();
+
+console.log(gameboard.winningSequences)
